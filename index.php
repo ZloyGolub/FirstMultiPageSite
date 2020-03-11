f35b6fbf6a4c9afe83296692cc1b2c2f7807a02c
ZloyGolub <zloygolub@gmail.com>
2020-03-11 20:37:21 +0200

    Before adding parcel

--- index.php
+++ index.php
@@ -12,44 +12,14 @@
 </head>
 
 <body>
-    <div class="border-bottom border-5">
-        <div class="container"> 
-            <div class="top">
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-3 col-lg-6 col-xl-6"> 
-                        <div class="topForFlexLeft">
-                            <img src="images/logos/facebook-logo.svg" height="20px" alt="">
-                            <img src="images/logos/twitter.svg" height="20px" alt="">
-                            <img src="images/logos/linkedin.svg" height="20px" alt="">
-                            <img src="images/logos/google-plus.svg" height="20px" alt="">
-                        </div>
-                    </div>
-                    <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2" id="inerRightFlex">
-                        <div class="row" id="inerRight">
-                            <img src="images/logos/clock.svg" height="20px" alt="">
-                            <div class="row"> Mon - Sat: 7:00 - 17:00</div>
-                        </div>
-                    </div>
-                    
-                    <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2" id="inerRightFlex">
-                        <div class="row" id="inerRight">
-                            <img src="images/logos/phone.svg" height="20px" alt="">
-                            <div class="row"> +386 40 111 5555</div>
-                        </div>
-                    </div>
-                    
-                    <div class="col-12 col-sm-4 col-md-3 col-lg-2 col-xl-2" id="inerRightFlex">
-                        <div class="row" id="inerRight">
-                            <img src="images/logos/mail.svg" height="20px" alt="">
-                            <div class="row"> info@yourdomain.com</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
+    <?php 
+        include('top.php');
+    ?>
 
+    <?php
+        include('navigation.php');
+    ?>
 
     <div class="container">
 
---
+++ navigation.php
@@ -0,0 +1,19 @@
+<div class="container"> 
+    <div class="navigation">
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-3 align-self-center">
+            <img src="images/logos/checkbox-logo.png" alt="" height="150px">
+            </div>
+            <div class="col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3 align-self-center">
+                <div class="row" id="navigationSet">
+                    <a href="#" class="link">HOME</a>
+                    <a href="#" class="link">ABOUT US</a>
+                    <a href="#" class="link">PAGES</a>
+                    <a href="#" class="link">PROJECTS</a>
+                    <a href="#" class="link">BLOG</a>
+                    <a href="#" class="link">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

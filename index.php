f85dd836f42965976ee4a70383c2262443b99a03
ZloyGolub <zloygolub@gmail.com>
2020-03-13 18:55:02 +0200

    Slider

--- index.php
+++ index.php
@@ -20,15 +20,67 @@
     <?php
         include('navigation.php');
     ?>
+    <!--
+        <div id="slider">
+                <button class="sButtons" id="sideButtonLeft"><</button>
+                    <div class="container align-self-center">
+                            <div class="col-xl-6 offset-xl-3" id="bigLogo">We care about your business.</div>
+                            <div class="col-xl-6 offset-xl-3" id="infoLogo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, porro!</div>
+                            <button class="sButtons" id="aboutButton">ABOUT US</button>
+                    </div>
+            <button class="sButtons" id="sideButtonRight">></button>
+        </div>
+    -->
 
-    <div id="slider">
-            <button class="sButtons" id="sideButtonLeft"><</button>
-                <div class="container align-self-center">
-                        <div class="col-xl-6 offset-xl-3" id="bigLogo">We care about your business.</div>
-                        <div class="col-xl-6 offset-xl-3" id="infoLogo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, porro!</div>
-                        <button class="sButtons" id="aboutButton">ABOUT US</button>
+    <div id="backgroundUnder">
+        <div id="slider">
+            <input type="radio" name="slider" id="slide1" checked>
+            <input type="radio" name="slider" id="slide2">
+            <input type="radio" name="slider" id="slide3">
+            <input type="radio" name="slider" id="slide4">
+            <div id="slides">
+                <div id="overflow">
+                    <div class="inner">
+                        <div class="slide slide_1">
+                        <div class="slide-content">
+                            <h2>Slide 1</h2>
+                            <p>Content for Slide 1</p>
+                        </div>
+                        </div>
+                        <div class="slide slide_2">
+                        <div class="slide-content">
+                            <h2>Slide 2</h2>
+                            <p>Content for Slide 2</p>
+                        </div>
+                        </div>
+                        <div class="slide slide_3">
+                        <div class="slide-content">
+                            <h2>Slide 3</h2>
+                            <p>Content for Slide 3</p>
+                        </div>
+                        </div>
+                        <div class="slide slide_4">
+                        <div class="slide-content">
+                            <h2>Slide 4</h2>
+                            <p>Content for Slide 4</p>
+                        </div>
+                        </div>
+                    </div>
                 </div>
-           <button class="sButtons" id="sideButtonRight">></button>
+            </div>
+            <div id="controls">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+            <div id="bullets">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+        </div>
     </div>
 
     <div class="container">
